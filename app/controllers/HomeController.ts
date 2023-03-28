2618b7dabe6081f24c642089a8a85da776fa472b
Baptiste Buvron <baptiste.buvron@gmail.com>
2023-03-28 15:23:39 +0200

    Feats: Ajout ejs partials

--- app/controllers/HomeController.ts
+++ app/controllers/HomeController.ts
@@ -3,8 +3,8 @@ import express from "express";
 export class HomeController {
     public static index(req: express.Request, res: express.Response) {
         //Render ejs file
-         res.status(200).json('Hello World From the Typescript API!')
-        //res.render("index", { title: "Home" });
+        //res.status(200).json('Hello World From the Typescript API!')
+        res.render("index", { title: "Home" });
     }
 
 
--- app/index.ts
+++ app/index.ts
@@ -9,6 +9,9 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 app.use("/", defaultRoute);
 
