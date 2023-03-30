5db97c1eadd9ebd08b3db7dc1e2e5244e1ba3cc5
Soudarsane TILLAI <83591055+darsane21@users.noreply.github.com>
2023-03-30 15:00:20 +0200

    add data into database

--- app/table.sql
+++ app/table.sql
@@ -1,100 +1,25 @@
-DROP TABLE IF EXISTS Candidature;
-DROP TABLE IF EXISTS FicheDePoste;
-DROP TABLE IF EXISTS OffreDePoste;
-DROP TABLE IF EXISTS Organisation;
-DROP TABLE IF EXISTS Piece;
-DROP TABLE IF EXISTS Utilisateur;
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 30 mars 2023 à 14:58
+-- Version du serveur :  10.3.29-MariaDB-0+deb10u1
+-- Version de PHP : 7.3.29-1~deb10u1
 
--- --------------------------------------------------------
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Structure de la table `FicheDePoste`
+-- Base de données : `ai16p003`
 --
-
-CREATE TABLE `FicheDePoste` (
-        `numero` int(11) NOT NULL,
-        `status` varchar(64) NOT NULL,
-        `responsable` varchar(64) NOT NULL,
-        `type_metier` varchar(64) NOT NULL,
-        `lieu` varchar(64) NOT NULL,
-        `teletravail` tinyint(1) NOT NULL,
-        `nbheure` int(11) NOT NULL,
-        `salaire` varchar(64) NOT NULL,
-        `description` text NOT NULL,
-        PRIMARY KEY (numero)
-);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `OffreDePoste`
---
-
-CREATE TABLE `OffreDePoste` (
-        `numero` int(11) NOT NULL,
-        `etat` varchar(64) NOT NULL CHECK (etat IN ('non publiée', 'publiée', 'expiré')),
-        `date_validite` date NOT NULL,
-        `nb_piece` int(11) NOT NULL,
-        `liste_piece` text NOT NULL,
-        `siren` int(11) NOT NULL REFERENCES Organisation(siren),
-        `fiche` int(11) NOT NULL REFERENCES FicheDePoste(numero),
-        PRIMARY KEY (numero),
-        UNIQUE (siren)
-);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Organisation`
---
-
-CREATE TABLE `Organisation` (
-    `siren` int(11) NOT NULL,
-    `nom` varchar(64) NOT NULL,
-    `type` varchar(64) NOT NULL,
-    `siege` varchar(128) NOT NULL,
-    PRIMARY KEY (siren)
-);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Piece`
---
-
-CREATE TABLE `Piece` (
-     `id` int(11) NOT NULL,
-     `nom` varchar(128) NOT NULL,
-     `fichier` varchar(128) NOT NULL,
-     `candidature` int(11) NOT NULL REFERENCES Candidature(candidat),
-     PRIMARY KEY (id)
-);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Utilisateur`
---
-
-CREATE TABLE `Utilisateur` (
-       `mail` varchar(128) NOT NULL,
-       `nom` varchar(64) NOT NULL,
-       `prenom` varchar(64) NOT NULL,
-       `telephone` varchar(64) NOT NULL,
-       `date_creation` date NOT NULL,
-       `statut` tinyint(1) NOT NULL,
-       `password` varchar(128) NOT NULL,
-       `type` varchar(64) NOT NULL CHECK (type IN ('Candidat', 'Recruteur', 'Administrateur')),
-       `demandeOrganisation` varchar(64)  CHECK (demandeOrganisation IN ('En cours', 'accepté', 'refusé')),
-       `siren` int(11) REFERENCES Organisation(siren),
-       PRIMARY KEY (mail),
-
-       CHECK (
-           ((demandeOrganisation = 'En cours' OR demandeOrganisation = 'accepté' OR type = 'Recruteur') AND siren IS NOT NULL)
-           OR
-           ((demandeOrganisation = 'refusé' OR type = 'Administrateur' OR type = 'Candidat') AND siren IS NULL)
-        )
-);
 
 -- --------------------------------------------------------
 
@@ -103,8 +28,175 @@ CREATE TABLE `Utilisateur` (
 --
 
 CREATE TABLE `Candidature` (
-       `date_candidature` date NOT NULL,
-       `candidat` int(11) NOT NULL REFERENCES Utilisateur(mail),
-       `poste` int(11) NOT NULL REFERENCES OffreDePoste(numero),
-       PRIMARY KEY (candidat, poste)
-);
+  `date_candidature` date NOT NULL,
+  `candidat` int(11) NOT NULL,
+  `poste` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Candidature`
+--
+
+INSERT INTO `Candidature` (`date_candidature`, `candidat`, `poste`) VALUES
+('2023-03-30', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `FicheDePoste`
+--
+
+CREATE TABLE `FicheDePoste` (
+  `numero` int(11) NOT NULL,
+  `status` varchar(64) NOT NULL,
+  `responsable` varchar(64) NOT NULL,
+  `type_metier` varchar(64) NOT NULL,
+  `lieu` varchar(64) NOT NULL,
+  `teletravail` tinyint(1) NOT NULL,
+  `nbheure` int(11) NOT NULL,
+  `salaire` varchar(64) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `FicheDePoste`
+--
+
+INSERT INTO `FicheDePoste` (`numero`, `status`, `responsable`, `type_metier`, `lieu`, `teletravail`, `nbheure`, `salaire`, `description`) VALUES
+(1, '1', 'Henri', 'ingénieur', 'dijon', 1, 35, '35k', 'Poste de développeur full stack chef de projet, etc ..\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `OffreDePoste`
+--
+
+CREATE TABLE `OffreDePoste` (
+  `numero` int(11) NOT NULL,
+  `etat` varchar(64) NOT NULL CHECK (`etat` in ('non publiée','publiée','expiré')),
+  `date_validite` date NOT NULL,
+  `nb_piece` int(11) NOT NULL,
+  `liste_piece` text NOT NULL,
+  `siren` int(11) NOT NULL,
+  `fiche` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `OffreDePoste`
+--
+
+INSERT INTO `OffreDePoste` (`numero`, `etat`, `date_validite`, `nb_piece`, `liste_piece`, `siren`, `fiche`) VALUES
+(1, 'publiée', '2023-05-19', 2, 'CV LM', 123456, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Organisation`
+--
+
+CREATE TABLE `Organisation` (
+  `siren` int(11) NOT NULL,
+  `nom` varchar(64) NOT NULL,
+  `type` varchar(64) NOT NULL,
+  `siege` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Organisation`
+--
+
+INSERT INTO `Organisation` (`siren`, `nom`, `type`, `siege`) VALUES
+(123456, 'La banque', 'SARL', 'La défense');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Piece`
+--
+
+CREATE TABLE `Piece` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(128) NOT NULL,
+  `fichier` varchar(128) NOT NULL,
+  `candidature` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Piece`
+--
+
+INSERT INTO `Piece` (`id`, `nom`, `fichier`, `candidature`) VALUES
+(1, 'CV', 'nomfichier', 1),
+(2, 'Lettre de motivation', 'nomfichier2', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+CREATE TABLE `Utilisateur` (
+  `mail` varchar(128) NOT NULL,
+  `nom` varchar(64) NOT NULL,
+  `prenom` varchar(64) NOT NULL,
+  `telephone` varchar(64) NOT NULL,
+  `date_creation` date NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `type` varchar(64) NOT NULL CHECK (`type` in ('Candidat','Recruteur','Administrateur')),
+  `demandeOrganisation` varchar(64) DEFAULT NULL CHECK (`demandeOrganisation` in ('En cours','accepté','refusé')),
+  `siren` int(11) DEFAULT NULL
+) ;
+
+--
+-- Déchargement des données de la table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`mail`, `nom`, `prenom`, `telephone`, `date_creation`, `statut`, `password`, `type`, `demandeOrganisation`, `siren`) VALUES
+('tsoudar21@gmail.com', 'Til', 'Sou', '0652645299', '2023-03-30', 1, 'truc', 'Candidat', NULL, NULL);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Candidature`
+--
+ALTER TABLE `Candidature`
+  ADD PRIMARY KEY (`candidat`,`poste`);
+
+--
+-- Index pour la table `FicheDePoste`
+--
+ALTER TABLE `FicheDePoste`
+  ADD PRIMARY KEY (`numero`);
+
+--
+-- Index pour la table `OffreDePoste`
+--
+ALTER TABLE `OffreDePoste`
+  ADD PRIMARY KEY (`numero`),
+  ADD UNIQUE KEY `siren` (`siren`);
+
+--
+-- Index pour la table `Organisation`
+--
+ALTER TABLE `Organisation`
+  ADD PRIMARY KEY (`siren`);
+
+--
+-- Index pour la table `Piece`
+--
+ALTER TABLE `Piece`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  ADD PRIMARY KEY (`mail`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
