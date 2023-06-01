e82d2d9cc51c4446922cfc24991d84f4d56dcc21
Baptiste Buvron <baptiste.buvron@gmail.com>
2023-06-01 14:47:53 +0200

    Un recruteur peut voir une liste de candidature pour ses offres

---
+++ app/routes/RecruterRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import {RecruteurController} from "../controllers/RecruteurController";
+
+export const recruterRouter = express.Router();
+
+recruterRouter.get("/", RecruteurController.index);
+recruterRouter.get("/candidatures", RecruteurController.candidatures);
+
